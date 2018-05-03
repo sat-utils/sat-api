d62a60c400fa5cb8d9e128cd78b48be4b0300544
Matthew Hanson <matt.a.hanson@gmail.com>
2018-05-03 00:50:40 -0400

    cleanup and update library functions

--- lambdas/api/index.js
+++ lambdas/api/index.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const zlib = require('zlib')
-const api = require('sat-api-lib')
+const satlib = require('sat-api-lib')
 const util = require('lambda-proxy-utils')
 const get = require('lodash.get')
 const es = require('../../lib/es')
@@ -9,22 +9,6 @@ const es = require('../../lib/es')
 
 module.exports.handler = function (event, context, cb) {
   console.log(`API handler: ${JSON.stringify(event)}`)
-
-  const resources = event.resource.split('/')
-
-  switch (resources[0]) {
-    case 'api':
-      console.log('/api')
-      cb()
-      break
-    case 'collections':
-      console.log('/collections')
-      cb()
-      break
-    case 'search':
-      console.log('/search')
-      break
-  }
 
   const method = event.httpMethod
   const payload = { query: {}, headers: event.headers }
@@ -34,20 +18,47 @@ module.exports.handler = function (event, context, cb) {
   else if (method === 'GET' && event.queryStringParameters) {
     payload.query = event.queryStringParameters
   }
-    
-  es.client().then((esClient) => {
 
-    const search = new api.search(payload, esClient);
-    //const encoding = get(req, 'headers.Accept-Encoding', null);
-    
-    search.search(function (err, resp) {
-      if (err) {
-        console.log(err)
-        const res = new util.Response({ cors: true, statusCode: 400 })
-        return cb(null, res.send({ details: err.message }))
-      }
-      const res = new util.Response({ cors: true, statusCode: 200 })
-      return cb(null, res.send(resp))
-    }); 
-  })
+  function respond(err, resp) {
+    if (err) {
+      console.log(err)
+      const res = new util.Response({ cors: true, statusCode: 400 })
+      return cb(null, res.send({ details: err.message }))
+    }
+    const res = new util.Response({ cors: true, statusCode: 200 })
+    //return cb(null, res.send(JSON.stringify(resp, null, 2)))
+    return cb(null, res.send(resp))
+  }
+
+
+  const resources = event.resource.split('/')
+  console.log(resources)
+
+  switch (resources[1]) {
+    case 'api':
+      // this should return API doc
+      console.log('/api')
+      respond(null, 'TODO - return API doc')
+      break
+    case 'collections':
+      // collections api
+      console.log('/collections')
+      //respond(null, 'collections')
+      es.client().then((esClient) => {
+        const api = new satlib.api(payload, esClient)
+        api.search('collections', respond)
+      })
+      break
+    case 'search':
+      // items api
+      console.log('/search')
+      es.client().then((esClient) => {
+        const api = new satlib.api(payload, esClient)
+        //const encoding = get(req, 'headers.Accept-Encoding', null)
+        api.search('items', respond)
+      })
+
+      break
+  }
+
 }
