5ac026a2d66e0adcbd72a23d5a6fb84a79290a7e
Scisco <scisco7@gmail.com>
2018-07-17 13:32:28 -0400

    lint sentinel package

--- packages/sentinel/index.js
+++ packages/sentinel/index.js
@@ -1,136 +1,125 @@
-'use strict'
+'use strict';
 
-const got = require('got')
-const proj4 = require('proj4')
-const epsg = require('epsg')
-const kinks = require('turf-kinks')
-const range = require('lodash.range')
-const pad = require('lodash.padstart')
-const moment = require('moment')
-const through2 = require('through2')
-const satlib = require('@sat-utils/api-lib')
-
+const got = require('got');
+const proj4 = require('proj4');
+const epsg = require('epsg');
+const kinks = require('turf-kinks');
+const range = require('lodash.range');
+const pad = require('lodash.padstart');
+const moment = require('moment');
+const through2 = require('through2');
+const clone = require('lodash.clonedeep');
+const fromPairs = require('lodash.frompairs');
+const get = require('lodash.get');
+const satlib = require('@sat-utils/api-lib');
 
 const collection = {
-  "c:id": "sentinel-2-l1c",
-  "c:name": "Sentinel 2 L1C",
-  "c:description": "Sentinel-2a and Sentinel-2b imagery",
-  "provider": "ESA",
-  "license": "https://sentinel.esa.int/documents/247904/690755/Sentinel_Data_Legal_Notice",
-  "eo:gsd": 10,
-  "eo:instrument": "MSI",
-  "eo:off_nadir": 0,
-  "eo:bands": {
-    "B01": {
-      "common_name": "coastal",
-      "gsd": 60.0,
-      "center_wavelength": 0.4439,
-      "full_width_half_max": 0.027
+  'c:id': 'sentinel-2-l1c',
+  'c:name': 'Sentinel 2 L1C',
+  'c:description': 'Sentinel-2a and Sentinel-2b imagery',
+  provider: 'ESA',
+  license: 'https://sentinel.esa.int/documents/247904/690755/Sentinel_Data_Legal_Notice',
+  'eo:gsd': 10,
+  'eo:instrument': 'MSI',
+  'eo:off_nadir': 0,
+  'eo:bands': {
+    B01: {
+      common_name: 'coastal',
+      gsd: 60.0,
+      center_wavelength: 0.4439,
+      full_width_half_max: 0.027
     },
-    "B02": {
-      "common_name": "blue",
-      "gsd": 10.0,
-      "center_wavelength": 0.4966,
-      "full_width_half_max": 0.098
+    B02: {
+      common_name: 'blue',
+      gsd: 10.0,
+      center_wavelength: 0.4966,
+      full_width_half_max: 0.098
     },
-    "B03": {
-      "common_name": "green",
-      "gsd": 10.0,
-      "center_wavelength": 0.56,
-      "full_width_half_max": 0.045
+    B03: {
+      common_name: 'green',
+      gsd: 10.0,
+      center_wavelength: 0.56,
+      full_width_half_max: 0.045
     },
-    "B04": {
-      "common_name": "red",
-      "gsd": 10.0,
-      "center_wavelength": 0.6645,
-      "full_width_half_max": 0.038
+    B04: {
+      common_name: 'red',
+      gsd: 10.0,
+      center_wavelength: 0.6645,
+      full_width_half_max: 0.038
     },
-    "B05": {
-      "gsd": 20.0,
-      "center_wavelength": 0.7039,
-      "full_width_half_max": 0.019
+    B05: {
+      gsd: 20.0,
+      center_wavelength: 0.7039,
+      full_width_half_max: 0.019
     },
-    "B06": {
-      "gsd": 20.0,
-      "center_wavelength": 0.7402,
-      "full_width_half_max": 0.018
+    B06: {
+      gsd: 20.0,
+      center_wavelength: 0.7402,
+      full_width_half_max: 0.018
     },
-    "B07": {
-      "gsd": 20.0,
-      "center_wavelength": 0.7825,
-      "full_width_half_max": 0.028
+    B07: {
+      gsd: 20.0,
+      center_wavelength: 0.7825,
+      full_width_half_max: 0.028
     },
-    "B08": {
-      "common_name": "nir",
-      "gsd": 10.0,
-      "center_wavelength": 0.8351,
-      "full_width_half_max": 0.145
+    B08: {
+      common_name: 'nir',
+      gsd: 10.0,
+      center_wavelength: 0.8351,
+      full_width_half_max: 0.145
     },
-    "B8A": {
-      "gsd": 20.0,
-      "center_wavelength": 0.8648,
-      "full_width_half_max": 0.033
+    B8A: {
+      gsd: 20.0,
+      center_wavelength: 0.8648,
+      full_width_half_max: 0.033
     },
-    "B09": {
-      "gsd": 60.0,
-      "center_wavelength": 0.945,
-      "full_width_half_max": 0.026
+    B09: {
+      gsd: 60.0,
+      center_wavelength: 0.945,
+      full_width_half_max: 0.026
     },
-    "B10": {
-      "common_name": "cirrus",
-      "gsd": 60.0,
-      "center_wavelength": 1.3735,
-      "full_width_half_max": 0.075
+    B10: {
+      common_name: 'cirrus',
+      gsd: 60.0,
+      center_wavelength: 1.3735,
+      full_width_half_max: 0.075
     },
-    "B11": {
-      "common_name": "swir16",
-      "gsd": 20.0,
-      "center_wavelength": 1.6137,
-      "full_width_half_max": 0.143
+    B11: {
+      common_name: 'swir16',
+      gsd: 20.0,
+      center_wavelength: 1.6137,
+      full_width_half_max: 0.143
     },
-    "B12": {
-      "common_name": "swir22",
-      "gsd": 20.0,
-      "center_wavelength": 2.22024,
-      "full_width_half_max": 0.242
+    B12: {
+      common_name: 'swir22',
+      gsd: 20.0,
+      center_wavelength: 2.22024,
+      full_width_half_max: 0.242
     }
   }
-}
+};
 
 
 const awsBaseUrl = 'https://sentinel-s2-l1c.s3.amazonaws.com';
 
 
 function parseMgrs(mgrs) {
-  var vals = {
-    'utm_zone': parseInt(mgrs),
-    'latitude_band': mgrs.slice(mgrs.length - 3, mgrs.length - 2),
-    'grid_square': mgrs.slice(mgrs.length - 2, mgrs.length)
-  }
-  const alphaVal = vals['latitude_band'].toLowerCase().charCodeAt(0) - 97 + 1
+  const vals = {
+    utm_zone: parseInt(mgrs),
+    latitude_band: mgrs.slice(mgrs.length - 3, mgrs.length - 2),
+    grid_square: mgrs.slice(mgrs.length - 2, mgrs.length)
+  };
+  let alphaVal = vals.latitude_band.toLowerCase().charCodeAt(0);
+  alphaVal = (alphaVal - 97) + 1;
   if (alphaVal > 13) {
     // northern hemisphere
-    vals['epsg'] = `326${vals.utm_zone}`
-  } else {
-    vals['epsg'] = `327${vals.utm_zone}`
+    vals.epsg = `326${vals.utm_zone}`;
   }
-  return vals
+  else {
+    vals.epsg = `327${vals.utm_zone}`;
+  }
+  return vals;
 }
-
-
-function getProductUrl(date, productId) {
-  const url = [
-    awsBaseUrl,
-    'products',
-    date.format('YYYY'),
-    date.format('M'),
-    date.format('D'),
-    productId
-  ];
-
-  return url.join('/')
-}
-
 
 function getTilePath(date, parsedMgrs) {
   return [
@@ -164,19 +153,20 @@ function getSentinelInfo(url) {
 }
 
 
-function reproject(geojson) {
+function reproject(inputGeojson) {
+  let geojson = clone(inputGeojson);
   const crs = geojson.crs.properties.name.replace(
     'urn:ogc:def:crs:', ''
   ).replace('8.8.1:', '');
   const from = epsg[crs];
   //const to = proj4.default.defs('EPSG:4326', '+proj=longlat +datum=WGS84 +no_defs');
-  const to = '+proj=longlat +datum=WGS84 +over'
+  const to = '+proj=longlat +datum=WGS84 +over';
 
   //console.log(`${geojson.type} ${JSON.stringify(geojson)}`)
   if (geojson.type === 'Polygon') {
     geojson = {
       type: 'Polygon',
-      coordinates: [geojson.coordinates[0].map(c => proj4.default(from, to, c))]
+      coordinates: [geojson.coordinates[0].map((c) => proj4.default(from, to, c))]
     };
   }
   else if (geojson.type === 'Point') {
@@ -184,41 +174,41 @@ function reproject(geojson) {
       type: 'Point',
       coordinates: proj4.default(from, to, geojson.coordinates)
     };
-  } else {
-    throw Error(`cannot process non Point or Polygon geometries`)
+  }
+  else {
+    throw Error('cannot process non Point or Polygon geometries');
   }
   if (kinks(geojson).features.length > 0) {
-    throw Error(`self-intersecting polygon`)
+    throw Error('self-intersecting polygon');
   }
-  return geojson
+  return geojson;
 }
 
 
 function transform(data, encoding, next) {
-  const dt = moment(data.SENSING_TIME)
-  const mgrs = data.MGRS_TILE
-  const parsedMgrs = parseMgrs(mgrs)
-  const tilePath = getTilePath(dt, parsedMgrs)
-  const tileBaseUrl = getTileUrl(tilePath)
-  const bands = range(1, 13).map(i => pad(i, 3, 'B0'))
-  bands.push('B8A')
-  getSentinelInfo(`${tileBaseUrl}/tileInfo.json`).then((info) => {
-    info = info.body
-    const sat = info.productName.slice(0, 3)
-    const satname = `Sentinel-2${sat.slice(-1)}`
-    var val
-    var files = _.fromPairs(bands.map(function(b) {
-      val = {href: `${tileBaseUrl}/${b}.jp2`, "eo:bands": [b]}
-      return [b, val]
-    }))
-    files.thumbnail = {href: `${tileBaseUrl}/preview.jpg`}
-    files.tki = {href: `${tileBaseUrl}/TKI.jp2`, description: 'True Color Image'}
-    files.metadata = {href: `${tileBaseUrl}/metadata.xml`}
+  const dt = moment(data.SENSING_TIME);
+  const mgrs = data.MGRS_TILE;
+  const parsedMgrs = parseMgrs(mgrs);
+  const tilePath = getTilePath(dt, parsedMgrs);
+  const tileBaseUrl = getTileUrl(tilePath);
+  const bands = range(1, 13).map((i) => pad(i, 3, 'B0'));
+  bands.push('B8A');
+  getSentinelInfo(`${tileBaseUrl}/tileInfo.json`).then((body) => {
+    const info = body;
+    const sat = info.productName.slice(0, 3);
+    const satname = `Sentinel-2${sat.slice(-1)}`;
+    let val;
+    const files = fromPairs(bands.map((b) => {
+      val = { href: `${tileBaseUrl}/${b}.jp2`, 'eo:bands': [b] };
+      return [b, val];
+    }));
+    files.thumbnail = { href: `${tileBaseUrl}/preview.jpg` };
+    files.tki = { href: `${tileBaseUrl}/TKI.jp2`, description: 'True Color Image' };
+    files.metadata = { href: `${tileBaseUrl}/metadata.xml` };
     // reproject to EPSG:4326
-    var geom = reproject(info.tileDataGeometry)
-    const lons = geom['coordinates'][0].map((pt) => { return pt[0] })
-    const lats = geom['coordinates'][0].map((pt) => { return pt[1] })
-    const bbox = [Math.min(...lons), Math.min(...lats), Math.max(...lons), Math.max(...lats)]
+    const geom = reproject(info.tileDataGeometry);
+    const lons = geom.coordinates[0].map((pt) => pt[0]);
+    const lats = geom.coordinates[0].map((pt) => pt[1]);
     const record = {
       id: data.GRANULE_ID,
       bbox: [Math.min(...lons), Math.min(...lats), Math.max(...lons), Math.max(...lats)],
@@ -230,38 +220,50 @@ function transform(data, encoding, next) {
       'eo:epsg': parsedMgrs.epsg,
       assets: files,
       links: {},
-      'sentinel:product_id': data.PRODUCT_ID,
+      'sentinel:product_id': data.PRODUCT_ID
       //'sentinel:tile_geometry': reproject(info.tileGeometry),
       //'sentinel:tileOrigin': reproject(info.tileOrigin)
-    }
-    this.push(record)
-    next()
-  }).catch(e => {
+    };
+    this.push(record);
+    next();
+  }).catch((e) => {
     // don't want to break stream, just log and continue
-    console.log(`error processing ${data.GRANULE_ID}: ${e}`)
-    next()
-  })
+    console.log(`error processing ${data.GRANULE_ID}: ${e}`);
+    next();
+  });
 }
 
 
-function handler(event, context=null, cb=function(){}) {
-  console.log(JSON.stringify(event))
-  const bucket = _.get(event, 'bucket')
-  const key = _.get(event, 'key')
-  const currentFileNum = _.get(event, 'currentFileNum', 0)
-  const lastFileNum = _.get(event, 'lastFileNum', 0)
-  const arn = _.get(event, 'arn', null)
-  const retries = _.get(event, 'retries', 0)
-  var _transform = through2({'objectMode': true}, transform)
+function handler(event, context, cb) {
+  console.log(JSON.stringify(event));
+  const bucket = get(event, 'bucket');
+  const key = get(event, 'key');
+  const currentFileNum = get(event, 'currentFileNum', 0);
+  const lastFileNum = get(event, 'lastFileNum', 0);
+  const arn = get(event, 'arn', null);
+  const retries = get(event, 'retries', 0);
+  const _transform = through2({ objectMode: true }, transform);
 
   // add collection
-  satlib.es.client().then((client) => {
-    satlib.es.putMapping(client, 'collections').catch((err) => {})
-    satlib.es.saveRecords(client, [collection], index='collections', 'c:id', (err, updated, errors) => {
-      if (err) console.log('Error: ', err)
+  let esClient;
+  return satlib.es.client()
+    .then((client) => {
+      esClient = client;
+      return satlib.es.putMapping(esClient, 'collections');
     })
-    satlib.ingestcsv.update({client, bucket, key, transform:_transform, cb, currentFileNum, lastFileNum, arn, retries}) 
-  })
+    .then(() => satlib.es.saveRecords(esClient, [collection], 'collections', 'c:id'))
+    .then(() => satlib.ingestcsv.update({
+      esClient,
+      bucket,
+      key,
+      transform: _transform,
+      cb,
+      currentFileNum,
+      lastFileNum,
+      arn,
+      retries
+    }))
+    .catch((e) => console.log('Error: ', e));
 }
 
 module.exports.handler = handler;
