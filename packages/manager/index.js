3bc944f5754869e575cb58c2605520eeb384f6eb
Scisco <scisco7@gmail.com>
2018-07-17 13:36:34 -0400

    lint manager

--- packages/manager/index.js
+++ packages/manager/index.js
@@ -1,47 +1,52 @@
 'use strict';
 
-const satlib = require('@sat-utils/api-lib')
+const satlib = require('@sat-utils/api-lib');
 
-module.exports.handler = function (event, context, cb) {
-  satlib.es.client().then((client) => {
+module.exports.handler = function hander(event, context, cb) {
+  let esClient;
+  return satlib.es.client().then((client) => {
+    esClient = client;
     if (event.action === 'putMapping') {
-      return satlib.es.putMapping(client, event.index);
+      return satlib.es.putMapping(esClient, event.index);
     }
     else if (event.action === 'deleteIndex') {
-      return satlib.es.deleteIndex(client, event.index);
+      return satlib.es.deleteIndex(esClient, event.index);
     }
     else if (event.action === 'listIndices') {
-      return satlib.es.listIndices(client, event.index);
+      return satlib.es.listIndices(esClient, event.index);
     }
     else if (event.action === 'reindex') {
-      return satlib.es.reindex(client, event.source, event.dest);
+      return satlib.es.reindex(esClient, event.source, event.dest);
     }
     else if (event.action === 'createRepo') {
-      let settings = {
+      const settings = {
         bucket: process.env.BUCKET,
-        role_arn: process.env.ROLE_ARN,
+        role_arn: process.env.ROLE_ARN
+      };
+      if (process.env.AWS_REGION === 'us-east-1') {
+        settings.endpoint = 's3.amazonaws.com';
       }
-      if (process.env.AWS_REGION == 'us-east-1') {
-        settings.endpoint = 's3.amazonaws.com'
-      } else {
-        settings.region = process.env.AWS_REGION
+      else {
+        settings.region = process.env.AWS_REGION;
       }
-      console.log('settings', settings)
-      return client.snapshot.createRepository({
+      console.log('settings', settings);
+      return esClient.snapshot.createRepository({
         repository: 'sat-api',
-        body: {type: 's3', settings},
-      })
-    } else if (event.action === 'takeSnapshot') {
-      client.snapshot.create({
+        body: { type: 's3', settings }
+      });
+    }
+    else if (event.action === 'takeSnapshot') {
+      return esClient.snapshot.create({
         repository: 'sat-api',
         snapshot: event.snapshot,
         body: {
           indices: 'collections,items'
         }
-      }).then(resp => {
-        console.log(resp)
-      })
+      }).then(console.log);
     }
-  }).then(r => cb(null, r))
-    .catch(e => cb(e));
+
+    return cb('No supported action was found');
+  })
+    .then((r) => cb(null, r))
+    .catch(cb);
 };
