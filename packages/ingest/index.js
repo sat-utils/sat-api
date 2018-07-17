cb32fb8ec37c62332b79cab493bfd85f6af0acfc
Scisco <scisco7@gmail.com>
2018-07-17 13:47:46 -0400

    lint ingest

--- packages/ingest/index.js
+++ packages/ingest/index.js
@@ -1,6 +1,7 @@
-'use strict'
-const satlib = require('@sat-utils/api-lib')
-const _ = require('lodash')
+'use strict';
+
+const satlib = require('@sat-utils/api-lib');
+const _ = require('lodash');
 
 /* Example Handler
 {
@@ -10,28 +11,39 @@ const _ = require('lodash')
 
 }
 */
+module.exports.handler = function handler(event, context, cb) {
+  console.log('ingest event: ', JSON.stringify(event));
+  const sat = event.satellite;
+  const bucket = process.env.bucket || 'sat-api';
+  let key = process.env.prefix || 'sat-api-dev';
+  key = `${key}/ingest/${sat}/`;
+  const maxFiles = _.get(event, 'maxFiles', 0);
+  const linesPerFile = _.get(event, 'linesPerFile', 500);
+  const maxLambdas = _.get(event, 'maxLambdas', 30);
+  const arn = _.get(event, 'arn', '');
 
-module.exports.handler = function (event, context, cb) {
-  console.log('ingest event: ', JSON.stringify(event))
-  const sat = event.satellite
-  const bucket = process.env.bucket || 'sat-api'
-  let key = process.env.prefix || 'sat-api-dev'
-  key = `${key}/ingest/${sat}/`
-  const maxFiles = _.get(event, 'maxFiles', 0)
-  const linesPerFile = _.get(event, 'linesPerFile', 500)
-  const maxLambdas = _.get(event, 'maxLambdas', 30)
-  const arn = _.get(event, 'arn', '')
-  
-  let url
-  let reverse = true
+  let url;
+  const reverse = true;
   switch (sat) {
-    case 'landsat':
-      url = 'https://landsat.usgs.gov/landsat/metadata_service/bulk_metadata_files/LANDSAT_8_C1.csv'
-      break
-    case 'sentinel':
-      url = 'https://storage.googleapis.com/gcp-public-data-sentinel-2/index.csv.gz'
-      break
+  case 'landsat':
+    url = 'https://landsat.usgs.gov/landsat/metadata_service/bulk_metadata_files/LANDSAT_8_C1.csv';
+    break;
+  case 'sentinel':
+    url = 'https://storage.googleapis.com/gcp-public-data-sentinel-2/index.csv.gz';
+    break;
+  default:
+    return cb('no match was found for satellite');
   }
 
-  satlib.ingestcsv.split({url, bucket, key, arn, maxFiles, linesPerFile, maxLambdas, reverse, cb})
+  return satlib.ingestcsv.split({
+    url,
+    bucket,
+    key,
+    arn,
+    maxFiles,
+    linesPerFile,
+    maxLambdas,
+    reverse,
+    cb
+  });
 }
