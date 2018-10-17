9f7ebb23c7adb04cec4475c02c502d6e623f7288
Matthew Hanson <matt.a.hanson@gmail.com>
2018-10-17 16:39:31 -0400

    update links, fix getting single item

--- packages/api-lib/libs/api.js
+++ packages/api-lib/libs/api.js
@@ -6,7 +6,15 @@ const geojsonError = new Error('Invalid GeoJSON Feature or geometry')
 const stac_version = '0.6.0'
 
 
-function STAC(path, endpoint, query, backend, page=1, limit=100, respond=()=>{}) {
+function STAC(path, endpoint, query, backend, respond=()=>{}) {
+
+  // default page and limit
+  var page = parseInt(query.page) || 1
+  var limit = parseInt(query.limit) || 1
+  delete query.page
+  delete query.limit
+
+  console.log(`Query page=${page}, limit=${limit}`)
 
   // split and remove empty strings
   const resources = path.split('/').filter((x) => x)
@@ -28,13 +36,13 @@ function STAC(path, endpoint, query, backend, page=1, limit=100, respond=()=>{})
     const catalog = {
       id: 'sat-api',
       description: 'A STAC API of public datasets',
-      'satapi:version': '1.0.0b1',
-      stac_version: '0.6.0',
+      'satapi:version': stac_version,
+      stac_version: stac_version,
       links: [
         { rel: 'self', href: `${endpoint}/stac` }
       ]
     }
-    const api = new API(backend, query, endpoint, page, limit=100)
+    const api = new API(backend, query, endpoint)
     api.search_collections((err, results) => {
       if (err) respond(err)
       for (let c of results.collections) {
@@ -59,7 +67,6 @@ function STAC(path, endpoint, query, backend, page=1, limit=100, respond=()=>{})
         const api = new API(backend, query, endpoint)
         api.search_collections(respond)
       } else if (resources.length === 2) {
-        console.log('get collection')
         // specific collection
         const api = new API(backend, query, endpoint)
         api.get_collection(resources[1], respond)
@@ -69,6 +76,10 @@ function STAC(path, endpoint, query, backend, page=1, limit=100, respond=()=>{})
         query['collection'] = resources[1]
         const api = new API(backend, query, endpoint)
         api.search_items(page, limit, respond)
+      } else if (resources[2] == 'item' && resources.length == 4) {
+        // Get specific item in collection
+        const api = new API(backend, query, endpoint)
+        api.get_item(resources[3], respond)
       } else {
         msg = 'endpoint not defined'
         console.log(msg, resources)
@@ -148,7 +159,7 @@ API.prototype.search_collections = function (callback) {
       // root catalog
       arr[i].links.push({rel: 'root', href: `${this.endpoint}/stac`})
       // child items
-      arr[i].links.push({rel: 'child', href: `${this.clink}/${a.id}/items`})
+      arr[i].links.push({rel: 'items', href: `${this.clink}/${a.id}/items`})
     })
 
     resp.collections = resp.results
@@ -179,35 +190,81 @@ API.prototype.search_items = function (page=1, limit=1, callback) {
   // check collection first
   this.search_collections((err, resp) => {
     const collections = resp.collections.map((c) => c.id)
-    if (collections.lenth === 0) {
+    if (collections.length === 0) {
       let resp = {
         type: 'FeatureCollection',
-        properties: { found: 0, limit: limit, page: page },
+        meta: { found: 0, returned: 0, limit: limit, page: page },
         features: []
       }
       callback(null, resp)
     } else {
+      // shallow copy of this.params
+      var params = { ...this.params }
       this.params['collection'] = collections.join(',')
 
       this.backend.search(this.params, 'items', page, limit, (err, resp) => {
         resp.results.forEach((a, i, arr) => {
           // self link
-          arr[i].links.splice(0, 0, {rel: 'self', href: `${this.endpoint}/stac/search?id=${a.id}`})
+          arr[i].links.splice(0, 0, {
+            rel: 'self',
+            href: `${this.clink}/${a.properties.collection}/item/${a.id}`
+          })
           // parent link
-          if (_.has(a.properties, 'collection')) {
-            arr[i].links.push({rel: 'parent', href: `${this.clink}/${a.properties.collection}`})
-          }
+          arr[i].links.push({rel: 'parent', href: `${this.clink}/${a.properties.collection}`})
+          arr[i].links.push({rel: 'collection', href: `${this.clink}/${a.properties.collection}`})
           arr[i].links.push({rel: 'root', href: `${this.endpoint}/stac`})
           arr[i]['type'] = 'Feature' 
         })
-        resp.type = 'FeatureCollection'
+        resp.type = 'FeatureCollection',
         resp.features = resp.results
         delete resp.results
+        // add next link if not last page
+        if ((page * limit) < resp.meta.found) {
+          params.page = page + 1
+          params.limit = limit
+          resp.links = [{
+              rel: 'next', title: 'Next page of results', 
+              href: `${this.endpoint}/stac/search?` + dictToURI(params)
+          }]
+        }
         callback(null, resp)
       })
     }
   })
 }
+
+
+API.prototype.get_item = function(id, callback) {
+  this.backend.search({id}, 'items', 1, 1, (err, resp) => {
+    if (resp.results.length === 1) {
+        var item = resp.results[0]
+        // self link
+        item.links.splice(0, 0, {
+          rel: 'self',
+          href: `${this.clink}/${item.properties.collection}/item/${item.id}`
+        })
+        // parent link
+        item.links.push({rel: 'parent', href: `${this.clink}/${item.properties.collection}`})
+        item.links.push({rel: 'collection', href: `${this.clink}/${item.properties.collection}`})
+        item.links.push({rel: 'root', href: `${this.endpoint}/stac`})
+        item['type'] = 'Feature'
+      callback(err, item)
+    } else {
+      callback(err, {})
+    }
+  })
+}
+
+
+// h/t https://medium.com/@mattccrampton/convert-a-javascript-dictionary-to-get-url-parameters-b77da8c78ec8
+function dictToURI(dict) {
+  var str = [];
+  for(var p in dict){
+     str.push(encodeURIComponent(p) + "=" + encodeURIComponent(dict[p]));
+  }
+  return str.join("&");
+}
+
 
 module.exports.stac_version = stac_version
 module.exports.STAC = STAC
