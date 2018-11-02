e11caa049266a2a8d02cf72aff60de5b103c80d8
Matthew Hanson <matt.a.hanson@gmail.com>
2018-11-02 17:08:36 -0400

    fix linting error

--- packages/api-lib/libs/es.js
+++ packages/api-lib/libs/es.js
@@ -34,7 +34,8 @@ async function connect() {
     }))
 
     AWS.config.update({
-      credentials: new AWS.Credentials(process.env.AWS_ACCESS_KEY_ID, process.env.AWS_SECRET_ACCESS_KEY),
+      credentials: new AWS.Credentials(process.env.AWS_ACCESS_KEY_ID,
+        process.env.AWS_SECRET_ACCESS_KEY),
       region: process.env.AWS_REGION || 'us-east-1'
     })
 
