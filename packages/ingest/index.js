6cba310c74d9f49417627ad53ba5c50e5071e9a1
Matthew Hanson <matt.a.hanson@gmail.com>
2018-11-07 18:01:38 -0500

    ingest SNS message lambda

--- packages/ingest/index.js
+++ packages/ingest/index.js
@@ -6,20 +6,21 @@ const satlib = require('@sat-utils/api-lib')
 
 // SNS message
 module.exports.handler = function handler(event, context, cb) {
-  console.log('ingest event: ', JSON.stringify(event))
   const msg = JSON.parse(event.Records[0].Sns.Message)
-  console.log(msg)
+  console.log('ingest message: ', JSON.stringify(msg))
   let url
   msg.Records.forEach((val) => {
     url = `https://${val.s3.bucket.name}.s3.amazonaws.com/${val.s3.object.key}`
-    console.log(`ingesting ${url}`)
 
     got(url, { json: true }).then((data) => {
       // create input stream from collection record
       const inStream = new readableStream.Readable({ objectMode: true })
-      inStream.push(data)
+      console.log(data.body)
+      inStream.push(data.body)
       inStream.push(null)
       return satlib.es.stream(inStream)
+    }).catch((err) => {
+      console.log(`Error ingesting: ${err}`)
     })
 
   })
