718b5decaf0f6e9bee4966adf6d2fcd8209565c0
sharkinsspatial <sharkinsgis@gmail.com>
2018-11-22 13:54:51 -0300

    Mock logger with proxyquire and test for Elasticsearch error.

--- packages/api-lib/tests/test_api_esSearch.js
+++ packages/api-lib/tests/test_api_esSearch.js
@@ -1,5 +1,6 @@
 const test = require('ava')
 const sinon = require('sinon')
+const proxquire = require('proxyquire')
 const api = require('../libs/apiNew')
 
 test('esSearch /stac', async (t) => {
@@ -24,6 +25,22 @@ test('esSearch /stac', async (t) => {
       href: 'endpoint/stac'
     }
   ]
+  t.is(search.firstCall.args[1], 'collections')
   t.deepEqual(actual.links, expectedLinks,
     'Returns STAC catalog with links to collections')
+})
+
+test('esSearch es error', async (t) => {
+  const error = sinon.spy()
+  const proxyApi = proxquire('../libs/apiNew', {
+    './logger': {
+      error
+    }
+  })
+  const errorMessage = 'errorMessage'
+  const search = sinon.stub().throws(new Error(errorMessage))
+  const backend = { search }
+  await proxyApi.esSearch('/stac', undefined, backend, 'endpoint')
+  t.is(error.firstCall.args[0].message, errorMessage,
+    'Logs Elasticsearch error via Winston transport')
 })
