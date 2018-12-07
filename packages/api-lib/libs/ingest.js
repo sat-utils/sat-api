f3c9bcd3a60f2c3014e4dd7b27ed1662c5632208
sharkinsspatial <sharkinsgis@gmail.com>
2018-12-07 18:57:27 -0300

    Refactor for recursive parameter and closure of increment variable.

--- packages/api-lib/libs/ingest.js
+++ packages/api-lib/libs/ingest.js
@@ -5,67 +5,19 @@ const Bottleneck = require('bottleneck')
 const isUrl = require('is-url')
 const util = require('util')
 const fs = require('fs')
-const { Duplex } = require('stream');
-const backend = require('./es')
-const pumpPromise = util.promisify(pump)
+const { Duplex } = require('stream')
 
+const completed = 'completed'
 const limiter = new Bottleneck({
-  maxConcurrent: 5000
+  maxConcurrent: 1000
 })
 const limitedRequest = limiter.wrap(request)
 const limitedRead = limiter.wrap(util.promisify(fs.readFile))
 
-let count = 0
-async function traverse(url, stream, root) {
-  count += 1
-  try {
-    let response
-    if (isUrl(url)) {
-      response = await limitedRequest(url)
-      count -= 1
-    } else {
-      response = await limitedRead(url)
-      count -= 1
-    }
-    const item = JSON.parse(response)
-    if (item) {
-      const written = stream.write(item)
-      if (written && item) {
-        traverseLinks(item, url, stream)
-      } else {
-        stream.once('drain', () => {
-          traverseLinks(item, url, stream)
-        })
-      }
-    }
-    if (count === 0 && !root) {
-      stream.write('completed')
-      return 'Completed'
-    }
-  } catch (error) {
-    count -= 1
-    console.log(error)
-  }
-}
-
-function traverseLinks(item, url, stream) {
-  const { links } = item
-  links.forEach(async (link) => {
-    const { rel, href } = link
-    if (rel === 'child' || rel === 'item') {
-      if (path.isAbsolute(href)) {
-        traverse(href, stream)
-      } else {
-        traverse(`${path.dirname(url)}/${link.href}`, stream)
-      }
-    }
-  })
-}
-
 class ItemStream extends Duplex {
-  constructor(options) {
+  constructor() {
     super({
-      readableObjectMode : true,
+      readableObjectMode: true,
       writableObjectMode: true,
       highWaterMark: 100
     })
@@ -78,11 +30,12 @@ class ItemStream extends Duplex {
     callback()
   }
   _read() {
+    // eslint-disable-next-line
     this.items.some((item, index, _items) => {
       if (item === 'completed') {
         this.push(null)
       } else {
-      console.log('Read ', item.id)
+        console.log('Read ', item.id)
         const pause = this.push(item)
         _items.splice(index, 1)
         return !pause
@@ -91,7 +44,64 @@ class ItemStream extends Duplex {
   }
 }
 
-async function ingest(url, recursive = true, collectionsOnly = false) {
+async function ingest(url, backend, recursive = true, collectionsOnly = false) {
+  let count = 0
+  // eslint-disable-next-line
+  async function traverse(url, stream, root) {
+    count += 1
+    try {
+      let response
+      if (isUrl(url)) {
+        response = await limitedRequest(url)
+        count -= 1
+      } else {
+        response = await limitedRead(url)
+        count -= 1
+      }
+      const item = JSON.parse(response)
+      if (item) {
+        const written = stream.write(item)
+        if (recursive) {
+          if (written && item) {
+            // eslint-disable-next-line
+            traverseLinks(item, url, stream, root)
+          } else {
+            stream.once('drain', () => {
+              // eslint-disable-next-line
+              traverseLinks(item, url, stream, root)
+            })
+          }
+        }
+      }
+      if ((count === 0 && !root) || (count === 0 && !recursive)) {
+        stream.write('completed')
+        return 'Completed'
+      }
+    } catch (error) {
+      count -= 1
+      console.log(error)
+    }
+  }
+
+  function traverseLinks(item, parentUrl, stream, root) {
+    const { links } = item
+    let hasChildren = false
+    links.forEach(async (link) => {
+      const { rel, href } = link
+      if (rel === 'child' || rel === 'item') {
+        hasChildren = true
+        if (path.isAbsolute(href)) {
+          traverse(href, stream)
+        } else {
+          traverse(`${path.dirname(parentUrl)}/${link.href}`, stream)
+        }
+      }
+    })
+    if (!hasChildren && root) {
+      stream.write(completed)
+    }
+  }
+
   const duplexStream = new ItemStream()
   await backend.prepare('collections')
   await backend.prepare('items')
@@ -106,8 +116,9 @@ async function ingest(url, recursive = true, collectionsOnly = false) {
       } else {
         console.log('Ingest complete')
       }
-    })
-  traverse(url, duplexStream, true)
+    }
+  )
+  traverse(url, duplexStream, true, recursive)
 }
 
 module.exports = { ingest }
