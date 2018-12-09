5b941784c442088f41c225d6e1cc33166a4d0cd5
sharkinsspatial <sharkinsgis@gmail.com>
2018-12-09 08:55:19 -0300

    Update integration ingest scripts with new parameters.

--- packages/api-lib/tests/integration/ingestData.js
+++ packages/api-lib/tests/integration/ingestData.js
@@ -1,9 +1,7 @@
 process.env.ES_HOST = 'http://192.168.99.100:4571'
 const ingest = require('../../libs/ingest').ingest
 const backend = require('../../libs/es')
-//const col = 'https://landsat-stac.s3.amazonaws.com/landsat-8-l1/226/88/catalog.json'
 
 //ingest('../fixtures/stac/catalog.json', backend)
-ingest('https://landsat-stac.s3.amazonaws.com/landsat-8-l1/226/70/catalog.json', backend)
-//ingest('https://landsat-stac.s3.amazonaws.com/landsat-8-l1/226/catalog.json', backend)
+ingest('https://landsat-stac.s3.amazonaws.com/landsat-8-l1/226/catalog.json', backend)
 
