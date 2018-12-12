1271e199a7283fe8a6d4557a8a0f2e7501164a81
sharkinsspatial <sharkinsgis@gmail.com>
2018-12-12 08:22:30 -0300

    Transform records to ElasticSearch bulk operations correctly.

--- packages/api-lib/libs/ElasticSearchWriteableStream.js
+++ packages/api-lib/libs/ElasticSearchWriteableStream.js
@@ -4,10 +4,10 @@ class ElasticSearchWritableStream extends stream.Writable {
   constructor(config, options) {
     super(options)
     this.config = config
+
     /**
      * Create the ElasticSearch client:
      */
-
     this.client = this.config.client
   }
 
@@ -15,34 +15,10 @@ class ElasticSearchWritableStream extends stream.Writable {
     return this.client.close()
   }
 
-  /**
-   * When writing a single record, we use the index() method of
-   * the ES API:
-   */
-
-  async _write(record, enc, next) {
-    /**
-     * Push the object to ES and indicate that we are ready for the next one.
-     * Be sure to propagate any errors:
-     */
-
-    try {
-      await this.client.index({
-        index: record.index,
-        type: record.type,
-        body: record.body
-      })
-      next()
-      return
-    } catch (err) {
-      next(err)
-    }
-  }
-
-  async _writev(chunks, next) {
-    const body = chunks.reduce((bulkOperations, chunk) => {
-      const record = chunk.chunk
+  transformRecords(chunks) {
+    const operations = chunks.reduce((bulkOperations, chunk) => {
       const operation = {}
+      const { chunk: record } = chunk
       operation[record.action] = {
         _index: record.index,
         _type: record.type,
@@ -51,17 +27,43 @@ class ElasticSearchWritableStream extends stream.Writable {
       if (record.parent) {
         operation[record.action]._parent = record.parent
       }
+
       bulkOperations.push(operation)
       if (record.action !== 'delete') {
         bulkOperations.push(record.body)
       }
       return bulkOperations
     }, [])
+    return operations
+  }
+  /**
+   * When writing a single record, we use the index() method of
+   * the ES API:
+   */
+
+  async _write(record, enc, next) {
+    try {
+      await this.client.index({
+        index: record.index,
+        type: record.type,
+        body: record.body.doc
+      })
+      next()
+    } catch (err) {
+      next(err)
+    }
+  }
+
+  async _writev(records, next) {
+    const body = this.transformRecords(records)
+
+    /**
+     * Push the array of actions to ES and indicate that we are ready
+     * for more data. Be sure to propagate any errors:
+     */
 
     try {
-      await this.client.bulk({
-        body
-      })
+      await this.client.bulk({ body })
       console.log('Wrote ', body.length)
       next()
     } catch (err) {
